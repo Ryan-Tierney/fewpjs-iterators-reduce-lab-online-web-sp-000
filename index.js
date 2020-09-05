26dc547fa08aafd43fdb40e2655dcb9e44ed2930
Ryan William Tierney <ryan.tierney92@gmail.com>
2020-09-05 16:15:04 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 // Code your solution here
-function batteries(batterBatches) { 
+function batteries(batteryBatches) { 
   var totalBatteries = 0; 
   batteryBatches.forEach(batterBatch => { 
     totalBatteries += batteryBatches;
